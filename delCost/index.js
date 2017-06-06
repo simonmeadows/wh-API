96d0008a2c0dc84922b1b63c871dcb8a0af2780d
Simon Meadows <Simon.Meadows@warehousesound.co.uk>
2017-06-06 16:13:18 +0100

    test as strings

--- delCost/index.js
+++ delCost/index.js
@@ -20,7 +20,7 @@ module.exports = function (context, req) {
                 context.res = {
                     // status: 200, /* Defaults to 200 */
 
-                    body: {edDist:20, glDist:50}
+                    body: {edDist:"20", glDist:"50"}
                     
                 };
                 context.done();
